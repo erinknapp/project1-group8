b454cbf3af2388bc3e41e173a07517b6263d3059
coleleg <coleleg@gmail.com>
2021-11-28 17:45:43 -0500

    linked script to index.html and added GeoDB test call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,26 @@
-const apiKeyYelp = "qFwMRrFz89YuZqRfKo1Def_bbMCDnBRebVF0aKvoW7-2SjGoHLVH63DTd2_QdE_L-Z-o--y_IsCEFShisIuQ9zj6ecoo8r6-XXsOnPYuW4eUYF88i_HUQd80oSmgYXYx"
+// const apiKeyYelp = "qFwMRrFz89YuZqRfKo1Def_bbMCDnBRebVF0aKvoW7-2SjGoHLVH63DTd2_QdE_L-Z-o--y_IsCEFShisIuQ9zj6ecoo8r6-XXsOnPYuW4eUYF88i_HUQd80oSmgYXYx"
 
-const apiKeyGoogle = "AIzaSyALFfph2f0J2EJwVFIHoNS_YVxXsA6P2Mg"
+// const apiKeyGoogle = "AIzaSyALFfph2f0J2EJwVFIHoNS_YVxXsA6P2Mg"
 
 
-let placesToEat = {
-    apiKey: "",
-    fetchPlacesToEat: function ()
+// let placesToEat = {
+//     apiKey: "",
+//     fetchPlacesToEat: function ()
+// }
+
+var getPopOver10M = function () {fetch("https://wft-geo-db.p.rapidapi.com/v1/geo/cities?minPopulation=10000", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
+		"x-rapidapi-key": "SIGN-UP-FOR-KEY"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
 }
+
+getPopOver10M();
